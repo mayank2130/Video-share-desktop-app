585a6b5818c55c7ddb342b123a8a5b3e7c6df1d2
mayank2130 <mayankthakur1712@gmail.com>
2024-11-08 06:50:03 +0530

    user profile type

---
+++ src/types/index.type.ts
@@ -0,0 +1,25 @@
+export type UserProfile = {
+    status: number;
+    user:
+      | ({
+          subscription: {
+            plan: "PRO" | "FREE";
+          } | null;
+          studio: {
+            id: string;
+            screen: string | null;
+            mic: string | null;
+            preset: "HD" | "SD";
+            camera: string | null;
+            userId: string | null;
+          } | null;
+        } & {
+          id: string;
+          email: string;
+          firstname: string | null;
+          lastname: string | null;
+          createdAt: Date;
+          clerkId: string;
+        })
+      | null;
+  } | null
